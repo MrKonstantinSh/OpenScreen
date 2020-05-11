4263964dea49ab1896af6f6ff2c060f9a4c16052
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-05-11 14:32:26 +0300

    feat: realize the ability to transfer images at 120 fps

--- OpenScreen.Core/Screenshot/Fps.cs
+++ OpenScreen.Core/Screenshot/Fps.cs
@@ -1,7 +1,11 @@
 ﻿namespace OpenScreen.Core.Screenshot
 {
+    /// <summary>
+    /// Provides an enumeration of the possible number of frames per second.
+    /// </summary>
     public enum Fps
     {
+        OneHundredAndTwenty = 8,
         Sixty = 16,
         Thirty = 33,
         Fifteen = 66
