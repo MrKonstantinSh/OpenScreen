8d9f5e4da030974aacb1e52e6a601e7f74e3849e
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-05-17 11:45:58 +0300

    chore: add application project files for screen streaming

---
+++ OpenScreen/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using OpenScreen.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OpenScreen.Core.Screenshot;
+using OpenScreen.Core.Server;
+
+namespace OpenScreen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var server = new StreamingServer(Resolution.Resolutions.SevenHundredAndTwentyP,
+                Fps.OneHundredAndTwenty, true);
+            server.Start(8080);
+        }
+    }
+}
